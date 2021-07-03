344d35ba2a520e8a025d1d0f9d78ffdd1413aaf3
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-07-03 22:55:53 +0000

    updates for class

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
 from SimpleGame.Scene import Scene
 from SimpleGame.Sprite import Sprite
+from SimpleGame.Block import Block
 import sys
 from PyQt5.QtWidgets import QApplication
+from enum import Enum
+
+class States(Enum):
+	FALLING = 0
+	WALK = 1
+	JUMP = 2
+	STAND = 3
+
+class Facing(Enum):
+	RIGHT = 0
+	LEFT = 1
+
 
 app = QApplication(sys.argv)
 
+
+
+
+class Ground(Block):
+	def __init__(self, thisScene):
+		spriteMaker = [["sprites/ground.png"] ] *30
+		super().__init__(thisScene, spriteMaker, 120, 40)
+		self.x = 0
+		self.y = 500
+		
+	def update(self):
+				
+		super().update()
+
 class Character(Sprite):
-	def __init__(self, thisScene, imageFile, xSize, ySize):
-		super().__init__(thisScene, imageFile, xSize, ySize)
+	def __init__(self, thisScene, sprite, x, y):
+		self.state = States.FALLING
+		self.facing = Facing.RIGHT
+		super().__init__(thisScene, sprite, x, y)
+		self.stateTimer = 0
+		self.dy = 7 
+		
+	def update(self):
+		if self.state == States.FALLING:
+			if self.scene.ground.collidesWith(self):
+				self.y = self.scene.ground.y - (self.height/2 + self.scene.ground.height / 2)
+				self.standBehavior
+		elif self.state == States.STAND or self.state == States.WALK:
+			if self.scene.keyDown[Scene.K_SPACE]:
+				self.jumpBehavior
+		elif self.state == States.STAND:
+			if self.scene.keysDown[Scene.K_RIGHT] or self.scene.keysDown[Scene.K_LEFT]:
+				self.walkBehavior
+		elif self.state == States.WALK:
+			if (self.facing == facing.RIGHT) and (self.scene.keysDown[Scene.K_RIGHT] != True):
+				self.standBehavior
+			if (self.facing == facing.LEFT) and (self.scene.keysDown[Scene.K_LEFT] != True):
+				self.standBehavior
+		elif self.state == States.JUMP:
+			self.stateTimer -= 1
+			if self.stateTimer < 1:
+				self.dy = self.dy * -1
+				self.state = States.FALLING
+		super().update()
 
 
 # sean mahady
@@ -25,13 +79,18 @@ class Sean(Character):
 		self.setAnimationSpeed(100)	#sets a QTimer to 100ms
 		self.playAnimation()	#starts the QTimer
 
+		#make a state for you class
+		self.state = States.FALLING	#falling
+
 	def update(self):
-		if self.y > 400:
-			self.dy = 0
-			self.y = 400
+		# add collision
+		self.scene.ground.collidesWith(self)
+		if self.state = States.FALLING:
+			self.dy = 1
+		
 		super().update()	
 
-# newton's sprite
+'''# newton's sprite
 # 88x64
 # 29x32
 #Newton
@@ -46,26 +105,66 @@ class slimy(Character):
 		self.generateAnimationCycles()
 		self.setAnimationSpeed(100)
 		self.playAnimation()
+		self.state = 0
 	def update(self):
 		if self.y > 400:
 			self.dy = 0
 			self.y = 400
-		super().update()
+		super().update()'''
 		
 
 # Animal
+#Sheet 480 x 240
+# Cell: 80x60
 class IEATTERMITES(Character):
   def __init__(self,thisScene):
-    super().__init__(thisScene, "sprites/niuniu_sprite.png",82,65)
+    super().__init__(thisScene, "sprites/niuniu_sprite.png", 480, 240)
     self.x = 125
     self.y = 180
     self.dy = 10
     self.boundAction = Scene.WRAP
-  def update(self):
-    if self.y > 400:
-      self.dy = 0
-      self.y = 400
-    super().update()
+
+		# add loadAnimation, generateAnimation, setAnimationSpeed, and playAnimation methods
+		#loadAnimation(sheetX, sheetY, cellX, cellY)
+    self.loadAnimation(480, 240, 80, 60)
+    self.generateAnimation()
+    self.setAnimationSpeed(9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000099)
+    self.playAnimation()  
+	def update(self):
+		super().update()
+
+	# Add a method called walkBehavior. 
+	# This should check if self.scene.keysDown[K_RIGHT]is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and 10
+	# If not check if self.scene.keysDown[K_LEFT] is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and -10
+
+	# Add a method called jumpBehavior. This should set the dy to a negative number (moving up), and set the stateTimer to the number of frames before falling.
+
+# 112x173
+# Jiayang
+# Sheet: 484 x 346
+# Cell: 96 x 173
+class SQUID(Character):
+	def __init__(self, thisScene):
+		super().__init__(thisScene, "sprites/jiayang_sprite.png", 484, 346)
+		self.x = 150
+		self.y = 150
+		self.dx = 4
+		self.dy = 4
+		self.boundAction = Scene.WRAP
+
+		# add loadAnimation, generateAnimation, setAnimationSpeed, and playAnimation methods
+		self.loadAnimation(484, 346, 96, 173)
+		self.generateAnimation()
+		self.setAnimationSpeed(300)
+		self.playAnimation()
+	def update(self):
+		super().update()
+
+	# Add a method called walkBehavior. 
+	# This should check if self.scene.keysDown[K_RIGHT]is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and 10
+	# If not check if self.scene.keysDown[K_LEFT] is True. If so self.facing to Facing.RIGHT, self.setCurrentCycle to Facing.RIGHT, call the self.startAnimation method. Set the DX to a value between 0 and -10
+
+	# Add a method called jumpBehavior. This should set the dy to a negative number (moving up), and set the stateTimer to the number of frames before falling.
     
     
 
@@ -86,33 +185,23 @@ class MEEEEEE(Character):
 		self.generateAnimationCycles()
 		self.setAnimationSpeed(100)
 		self.playAnimation()
+		self.state = States.FALLING
 	def update(self):
-		if self.y > 400:
-			self.dy = 0
-			self.y = 400
+		self.scene.ground.collidesWith(self)
+		if self.state = States.FALLING:
+		self.dy = `1
 		super().update()
 
-# 112x173
-#Jiayang
-class SQUID(Character):
-	def __init__(self, thisScene):
-		super().__init__(thisScene, "sprites/jiayang_sprite.png", 112, 173)
-		self.x = 150
-		self.y = 150
-		self.dx = 4
-		self.dy = 4
-		self.boundAction = Scene.WRAP
-	def update(self):
-		if self.y >400:
-			self.dy = 0
-			self.y = 400 
-		super().update()
+
 
 
 
 class Game(Scene):
 	def __init__(self):
 		super().__init__(600,600)
+
+		self.ground = Ground(self)
+
 		self.sean = Sean(self)
 		self.SQUID = SQUID(self)
 		self.MEEEEEE = MEEEEEE(self)
