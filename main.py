7918a4e32210ae1c969c015378ff4d110a26f650
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-07-04 00:59:48 +0000

    minor fix

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ class MEEEEEE(Character):
 	# This should check if self.scene.keysDown[Scene.K_RIGHT]is True. If so self.facing to 0, self.setCurrentCycle to 0, call the self.playAnimation method. Set the DX to a value between 0 and 10. Set a State to States.WALK
 	# If not check if self.scene.keysDown[Scene.K_LEFT] is True. If so self.facing to 1, self.setCurrentCycle to 1, call the self.playAnimation method. Set the DX to a value between 0 and -10. Set a State to States.WALK
 	def walkBehavior(self):
-		if self.scene.keysDown[Scene.K.RIGHT]:
+		if self.scene.keysDown[Scene.K_RIGHT]:
 			self.facing = 0
 			self.setCurrentSCycle(0)
 			self.playAnimation()
